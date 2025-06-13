50f4c01f66410d251a399e587167fa1f5b210f5d
Furkan <furkandereli98@gmail.com>
2025-06-13 11:57:36 +0300

    Create, update, delete and edit service result

--- App.Services/ServiceResult.cs
+++ App.Services/ServiceResult.cs
@@ -38,3 +38,38 @@ public class ServiceResult<T>
         };
     }
 }
+
+public class ServiceResult
+{
+    public List<string>? ErrorMessage { get; set; }
+    public bool IsSuccess => ErrorMessage == null || ErrorMessage.Count == 0;
+    public bool IsFail => !IsSuccess;
+    public HttpStatusCode Status { get; set; }
+
+    //static factory method
+    public static ServiceResult Success(HttpStatusCode status = HttpStatusCode.OK)
+    {
+        return new ServiceResult()
+        {
+            Status = status
+        };
+    }
+
+    public static ServiceResult Fail(List<string> errorMessage, HttpStatusCode status = HttpStatusCode.BadRequest)
+    {
+        return new ServiceResult()
+        {
+            ErrorMessage = errorMessage,
+            Status = status
+        };
+    }
+
+    public static ServiceResult Fail(string errorMessage, HttpStatusCode status = HttpStatusCode.BadRequest)
+    {
+        return new ServiceResult()
+        {
+            ErrorMessage = [errorMessage],
+            Status = status
+        };
+    }
+}
